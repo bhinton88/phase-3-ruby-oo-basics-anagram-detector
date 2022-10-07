b6aaaee69ad3ec39626253c5ee6ea2673d20e9e7
Bobby Hinton <bob.g.hint@gmail.com>
2022-10-06 21:11:13 -0500

    commit tree 5ad1c96a13f8501444186ca018f6c01e1a13bdba

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+
+  attr_reader :word
+  
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    matches = []
+    words.map do |word|
+      matches.push(word) if word.chars.sort == @word.chars.sort
+    end
+    matches
+  end
+end
+
+# we want it to iterate over every word in the words array
+# - then we want it to check if the word matches the word passed in on 
+# initialization 
+
+# if the word matches.. then we want to add it to the array
